96c3e8c0bd71356f3f17ed81e7be9a69e4cb6e3b
clam1t <egorskurenkov391@gmail.com>
2025-11-17 07:35:39 +0300

    1

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class Parser:
             text = page.get_text()
             all_text += text
 
-
         lines = [line.strip() for line in all_text.split('\n') if line.strip()]
 
         code_pattern = r'\d{2}\.[0][456]\.\d{2}'
@@ -32,9 +31,8 @@ class Parser:
             if date_match and not found_date:
                 found_date = date_match.group()
 
-
         if found_code and found_date:
-            doc_name = f"{found_code}_{found_date.replace(' ','_')}"
+            doc_name = f"{found_code}_{found_date.replace(' ', '_')}"
         elif found_code:
             doc_name = found_code
         elif found_date:
@@ -61,11 +59,25 @@ class Parser:
             start_pos = match1.end()
             end_pos = match2.start()
             disciplines = all_text[start_pos:end_pos].strip()
-            return disciplines
+            disciplines = [discipline.strip() for discipline in disciplines.split(',')]
+            all_disciplines = []
+            for i in range (len(disciplines)-1):
+                if "(" in disciplines[i]:
+                    disciplines[i]= str(disciplines[i])+" "+str(disciplines[i+1])
+                    disciplines.pop(i+1)
+            for discipline in disciplines:
+                match = re.search(r'\((.*?)\)', discipline)
+                if match:
+                    content = match.group(1)
+                    cleaned_content = ' '.join(content.split())
+                    all_disciplines.append(cleaned_content)
+
+            return list(dict.fromkeys(all_disciplines))
+
+
 
     def close(self):
         self.doc.close()
-
 
 
 parser = Parser("123.pdf")
